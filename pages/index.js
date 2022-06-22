5a342770d29d4d00535dfc4c0b64adc3954d2318
Fernando Ángel -Medellín Cuevas <fer0osennin@gmail.com>
2022-06-21 22:00:02 -0500

    start with aplication

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-
+import { useState,useEffect } from "react"
 
 export default function Home() {
   return (
     <div>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <input className="border-2 border-black m-2 p-2 rounded-sm" type='text' placeholder="Enter a new task" onChange={(e)=>console.log(e.target.value)}/>
+      <button onClick={()=> alert ('clicked')} className='m-2 border-2 border-black bg-blue-500 p-2 text-white rounded-sm'>Save new task</button>
     </div>
   )
 }
