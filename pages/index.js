9d90ad8f4e38e96e0220908b4731bd703714515f
Fernando Ángel -Medellín Cuevas <fer0osennin@gmail.com>
2022-06-21 22:01:52 -0500

    start with aplication

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useState,useEffect } from "react"
 
 export default function Home() {
+  const [newTaskName, setNewTaskName] = useState()
   return (
     <div>
-      <input className="border-2 border-black m-2 p-2 rounded-sm" type='text' placeholder="Enter a new task" onChange={(e)=>console.log(e.target.value)}/>
-      <button onClick={()=> alert ('clicked')} className='m-2 border-2 border-black bg-blue-500 p-2 text-white rounded-sm'>Save new task</button>
+      <input className="border-2 border-black m-2 p-2 rounded-sm" type='text' placeholder="Enter a new task" onChange={(e)=>setNewTaskName(e.target.value)}/>
+      <button onClick={()=> alert (newTaskName)} className='m-2 border-2 border-black bg-blue-500 p-2 text-white rounded-sm'>Save new task</button>
     </div>
   )
 }
